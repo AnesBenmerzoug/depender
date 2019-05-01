60dd18f6bf30940cee0f819bf28056a7758d2983
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2019-05-01 13:38:13 +0200

    Improve the tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,24 +12,56 @@ def graph() -> Graph:
                           |     |
                           +--+--+
                              |
-           +-----------------------+-----------+
+           +-----------+-----+-----+-----------+
            |           |           |           |
         +--+--+     +--+--+     +--+--+     +--+--+
         |  2  |     |  3  |     |  4  |     |  5  |
         |     |     |     |     |     |     |     |
         +--+--+     +-----+     +--+--+     +-----+
            |                       |
-           |           +-----------+
-           |           |           |
-        +--+--+     +--+--+     +--+--+
-        |  6  |     |  7  |     |  8  |
-        |     |     |     |     |     |
-        +-----+     +-----+     +-----+
+           |                 +-----+-----+
+           |                 |           |
+        +--+--+           +--+--+     +--+--+
+        |  6  |           |  7  |     |  8  |
+        |     |           |     |     |     |
+        +-----+           +-----+     +-----+
 
     Returns:
+        Graph object
 
     """
     graph = Graph()
+    graph.add_edge("1", "2")
+    graph.add_edge("1", "3")
+    graph.add_edge("1", "4")
+    graph.add_edge("1", "5")
+    graph.add_edge("2", "6")
+    graph.add_edge("4", "7")
+    graph.add_edge("4", "8")
+    return graph
+
+
+@pytest.fixture
+def graph_with_labels(graph: Graph) -> Graph:
+    r"""
+    Create a Graph objects that represents the same graph as the previous fixture
+    but with labels of different sizes
+
+    Returns:
+        Graph object
+
+    """
+    graph = Graph()
+    # Create the nodes first in order to set their labels
+    graph.add_node("1", label="one")
+    graph.add_node("2", label="two")
+    graph.add_node("3", label="three")
+    graph.add_node("4", label="four")
+    graph.add_node("5", label="five")
+    graph.add_node("6", label="six")
+    graph.add_node("7", label="seven")
+    graph.add_node("8", label="eight")
+    # Then connect them using edges
     graph.add_edge("1", "2")
     graph.add_edge("1", "3")
     graph.add_edge("1", "4")
