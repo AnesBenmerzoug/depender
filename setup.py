722e9cac5633a4da7bbb3121ef42a4f239f32dcc
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2019-05-30 12:16:17 +0200

    Remove bokeh from the requirement list

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 install_requires = [
     "click>=4.0",
     "click-spinner>=0.1.0",
-    "bokeh>=1.0.0",
     "matplotlib>=3.0.0",
     "plotly>=3.8.0",
     "Jinja2>=2.9"
