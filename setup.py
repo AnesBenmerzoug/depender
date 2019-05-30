b6a46f4e2528598d62713eefc5546abdf9765f8c
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2019-05-30 12:15:39 +0200

    Use matplotlib and plotly instead of bokeh

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ install_requires = [
     "click>=4.0",
     "click-spinner>=0.1.0",
     "bokeh>=1.0.0",
+    "matplotlib>=3.0.0",
+    "plotly>=3.8.0",
     "Jinja2>=2.9"
 ]
 
 setup(version="1.0",
       install_requires=install_requires,
       entry_points={
-        "console_scripts": ["depender=depender.main:main"],
+        "console_scripts": ["depender=depender.cli:main"],
       })
