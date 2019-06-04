5eb18d2e59732ee80b1ffef176cb97468cb66e08
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2019-06-04 15:36:18 +0200

    Add a size attribute and a __repr__ method to Node

--- depender/graph/node.py
+++ depender/graph/node.py
@@ -5,7 +5,7 @@ Attribute_Type = Optional[Union[str, float, "Node"]]
 
 
 class Node:
-    NodeProperties = ["name", "label", "x", "y", "width", "height", "index", "change",
+    NodeProperties = ["name", "label", "x", "y", "size", "width", "height", "index", "change",
                       "shift", "modifier", "thread", "parent", "ancestor",
                       "leftmost_sibling", "left_sibling", "right_sibling"]
 
@@ -15,6 +15,7 @@ class Node:
         self.type = kwargs.get("type", None)  # type: Optional[str]
         self.x = kwargs.get("x", 0)  # type: float
         self.y = kwargs.get("y", 0)  # type: float
+        self.size = kwargs.get("size", 0)  # type: float
         self.width = kwargs.get("width", 0)  # type: float
         self.height = kwargs.get("height", 0)  # type: float
         self.index = kwargs.get("index", 1)  # type: int
@@ -35,3 +36,9 @@ class Node:
         if key not in self.NodeProperties:
             raise AttributeError(f"'Node' object has no attribute '{key}'")
         setattr(self, key, value)
+
+    def __repr__(self):
+        return f"(Name: '{self.name}', Label: '{self.label}', \n" \
+            f"X: '{self.x}', Y: '{self.y}', Width: '{self.width}', Height: '{self.height}')"
+
+
