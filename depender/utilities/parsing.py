8d62e04d3a4db29eb837095e5b0d31edee270491
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2019-06-05 12:08:00 +0200

    Use pathlib in the parsing utilities and remove an unused function

--- depender/utilities/parsing.py
+++ depender/utilities/parsing.py
@@ -1,90 +1,72 @@
 import os
+from pathlib import Path
 from depender.graph.graph import Graph
-from typing import List, Tuple, Iterable, Optional
+from typing import List, Tuple, Iterable, Union
 
 
-def check_if_skip_directory(directory: str, excluded_directories: List[str]) -> bool:
-    skip = False
-    if directory in excluded_directories or "__pycache__" in directory:
-        skip = True
-    return skip
-
-
-def skip_hidden_directories(directories: List[str]) -> List[str]:
-    copy_of_directories = directories[:]
-    for directory in copy_of_directories:
-        if directory.startswith("."):
-            directories.remove(directory)
-    return directories
-
-
-def traverse_directory(root_directory: str,
-                       excluded_directories: List[str],
-                       depth: int,
-                       followlinks: bool,
-                       breadth_first: bool = False) -> Iterable[Tuple[str, List[str], List[str]]]:
-    root_depth = root_directory.count(os.path.sep)
-    dirlist = list()
-    for root, dirs, files in os.walk(root_directory, followlinks=followlinks):
-        # Check to see if there are user specified directories that should be skipped
-        if check_if_skip_directory(root, excluded_directories):
-            continue
-        # Don't go deeper than "depth" if it has a non-negative value
-        current_depth = root.count(os.path.sep) - root_depth
-        if current_depth > depth >= 0:
-            continue
-        dirs = skip_hidden_directories(dirs)
-        dirlist.append((root, dirs, files))
-        if breadth_first:
-            dirlist = sorted(dirlist, key=lambda x: x[0].count(os.path.sep))
-    for root, dirs, files in dirlist:
-        yield root, dirs, files
-
-
-def find_root_package(root_directory: str,
-                      excluded_directories: List[str],
-                      depth: int,
-                      followlinks: bool) -> Tuple[Optional[str], Optional[str]]:
-    package_root_path = None
-    package_name = None
-    for root, dirs, files in traverse_directory(root_directory,
-                                                excluded_directories,
-                                                depth=depth, followlinks=followlinks,
-                                                breadth_first=True):
-        if package_root_path is None:
-            if "__init__.py" in files:
-                package_root_path = root
-                package_name = os.path.basename(root)
-                break
-    return package_name, package_root_path
-
-
-def find_all_package_modules(package_root_path: str,
-                             package_name: str,
+def find_all_package_modules(package_path: Path,
+                             excluded_directories: List[Path],
                              graph: Graph,
-                             excluded_directories: List[str],
                              depth: int,
                              followlinks: bool):
     file_list = list()
-    for root, dirs, files in traverse_directory(package_root_path,
+    for root, dirs, files in traverse_directory(package_path,
                                                 excluded_directories,
                                                 depth=depth, followlinks=followlinks):
         for filename in files:
             # Skip non python files
-            if not filename.endswith(".py"):
+            if not ".py" == filename.suffix:
                 continue
-            # Skip .pyc and __init__.py files
-            if ".pyc" in filename or "__init__.py" in filename:
+            # Skip  __init__.py files
+            if "__init__.py" in filename.name:
                 continue
-
-            # Form the dot path relative to the package's root path
-            if len(package_root_path) > len(root):
-                module_dot_path = "." * (package_root_path.count(os.path.sep) - root.count(os.path.sep) + 1) \
-                                  + filename[:-3]
-            else:
-                module_dot_path = ".".join(filter(lambda x: bool(x), [package_name,
-                                                                      root[len(package_root_path) + 1:],
-                                                                      filename[:-3]]))
-            file_list.append((os.path.join(root, filename), module_dot_path))
+            # Form the dot path relative to the package's path
+            module_dot_path = root.relative_to(package_path.parent)
+            module_dot_path = module_dot_path.joinpath(filename.stem)
+            module_dot_path = ".".join(module_dot_path.parts)
+            file_list.append((root / filename, module_dot_path))
             graph.add_node(module_dot_path, label=module_dot_path)
     return file_list
+
+
+def traverse_directory(directory: Path,
+                       excluded_directories: List[Path],
+                       depth: int,
+                       followlinks: bool,
+                       breadth_first: bool = False) -> Iterable[Tuple[Path, List[Path], List[Path]]]:
+    root_depth = len(directory.parents)
+    dirlist = list()
+    for root, dirs, files in os.walk(directory, followlinks=followlinks):
+        root = Path(root)
+        # Check to see if there are user specified directories that should be skipped
+        if check_if_skip_directory(root, excluded_directories):
+            continue
+        # Don't go deeper than "depth" if it has a non-negative value
+        current_depth = len(root.parents) - root_depth
+        if current_depth > depth >= 0:
+            continue
+        # Convert the dir and filenames to Path instances
+        dirs = list(map(lambda dir: Path(dir), dirs))
+        files = list(map(lambda file: Path(file), files))
+        # Remove directories that start with a '.'
+        dirs = skip_hidden_directories(dirs)
+        dirlist.append((root, dirs, files))
+        # If breadth first is True, sort the paths by directory level
+        if breadth_first:
+            dirlist = sorted(dirlist, key=lambda x: len(x[0].parents))
+    for root, dirs, files in dirlist:
+        yield root, dirs, files
+
+
+def check_if_skip_directory(directory: Path, excluded_directories: List[Path]) -> bool:
+    if directory in excluded_directories or "__pycache__" in directory.name:
+        return True
+    else:
+        return False
+
+
+def skip_hidden_directories(directories: List[Path]) -> List[Path]:
+    for directory in directories[:]:
+        if str(directory)[0] == ".":
+            directories.remove(directory)
+    return directories
