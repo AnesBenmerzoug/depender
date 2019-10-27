c4402bcc58374898d1a4d7bb6d4f75b5ba7218b9
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2019-10-27 20:13:56 +0100

    Add abstract methods plot and save_to_file to BaseBackend

--- depender/backend/base.py
+++ depender/backend/base.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import Optional, Tuple
+from pathlib import Path
+from typing import Optional, Tuple, Union
 
 from depender.graph.dependency import DependencyGraph
 from depender.graph.structure import StructureGraph
@@ -8,18 +9,26 @@ from depender.graph.structure import StructureGraph
 class BaseBackend(ABC):
     def __init__(
         self,
-        output_dir: str,
-        output_format: Optional[str] = None,
+        output_dir: Union[str, Path],
+        format: Optional[str] = None,
         figure_dimensions: Tuple[float, float] = (1280, 960),
         dpi: int = 100,
     ) -> None:
-        self.output_dir = output_dir
-        self.output_format = output_format
+        self.output_dir = Path(output_dir)
+        self.format = format
         self.figure_dimensions = figure_dimensions
         self.dpi = dpi
 
     @abstractmethod
-    def plot_dependency_matrix(self, graph: DependencyGraph, **kwargs) -> None:
+    def plot(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def save_to_file(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def plot_dependency_matrix(self, graph: DependencyGraph, **kwargs):
         raise NotImplementedError
 
     @abstractmethod
